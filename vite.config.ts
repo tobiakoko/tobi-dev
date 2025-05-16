57bb76f1e2d20d505de6a29b1b0d2e26afcc9210
Daniel Akoko <tobiakoko@gmail.com>
2025-05-16 00:47:24 -0400

    update base url for production

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
   
-  
 export default defineConfig({
     plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
     base: process.env.NODE_ENV === 'production' ? '/tobi-dev/' : '/',
